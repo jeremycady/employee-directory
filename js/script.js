57a16c3a2e64c880fae89697bd734e3124dbd3c7
Jeremy Cady <jeremy@mx103.com>
2020-08-31 20:55:26 -0500

    createDirectory function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
-let employees;
+const search = document.querySelector('.search-container');
+const gallery = document.querySelector('#gallery');
 
 // fetch the employees from the Random User Generator API
-fetch('https://randomuser.me/api/?results=12&inc=name,picture,email,location&noinfo')
-                     .then(response => response.json())
-                     .then(data =>  employees = data.results);
+fetch('https://randomuser.me/api/?results=12&inc=name,picture,email,location&noinfo&nat=us')
+                     .then( response => response.json() )
+                     .then( data => createDirectory(data.results));
+
+// create the employee directory gallery
+const createDirectory = (employees) => {
+    console.log(employees);
+    function createElement(element, className) {
+        const newElement = document.createElement(element);
+        newElement.className = className;
+        return newElement;
+    };
+    
+    for (let employee of employees ) {
+        const card = createElement('div', 'card');
+        const imgContainer = createElement('div', 'card-img-container');
+        const img = createElement('img', 'card-img');
+        const infoContainer = createElement('div', 'card-info-container');
+        const name = createElement('h3', 'card-name cap');
+        const email = createElement('p', 'card-text');
+        const location = createElement('p', 'card-text cap');
+
+        img.setAttribute('alt', 'profile picture');
+        img.setAttribute('src', employee.picture.large);
+        name.textContent = `${employee.name.first} ${employee.name.last}`;
+        email.textContent = `${employee.email}`;
+        location.textContent = `${employee.location.city}, ${employee.location.state}`;
+        
+        imgContainer.appendChild(img);
+        infoContainer.appendChild(name);
+        infoContainer.appendChild(email);
+        infoContainer.appendChild(location);
+        card.appendChild(imgContainer);
+        card.appendChild(infoContainer);
+        gallery.appendChild(card);
+    }
+    
+};
