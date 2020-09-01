50307b5c98a897ffda7f9bf011aad58d850aac97
Jeremy Cady <jeremy@mx103.com>
2020-08-31 20:03:43 -0500

    fetch employee data

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+let employees;
+
+// fetch the employees from the Random User Generator API
+fetch('https://randomuser.me/api/?results=12&inc=name,picture,email,location&noinfo')
+                     .then(response => response.json())
+                     .then(data =>  employees = data.results);
